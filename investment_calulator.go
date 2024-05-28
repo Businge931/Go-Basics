3c27376f362857c1c803baf75e99a7cea0c6e246
Businge931 <jimmybusinge9@gmail.com>
2024-05-28 15:26:33 +0300

    18. Exercise Building a Profit Calculator

--- investment_calulator.go
+++ investment_calulator.go
@@ -2,28 +2,45 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main(){
-	const inflationRate =2.5
-	var investmentAmount float64 
-	var years float64
-	var expectedReturnRate float64
-    
-	fmt.Print("Investment Amount: ")
-	fmt.Scan(&investmentAmount)
+	// const inflationRate =2.5
+	// var investmentAmount float64 
+	// var years float64
+	// var expectedReturnRate float64
 
-	fmt.Print("years: ")
-	fmt.Scan(&years)
+	var taxRate float64
+	var revenue float64
+	var expenses float64
 
-	fmt.Print("Expected Return: ")
-	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax rate: ")
+	fmt.Scan(&taxRate)
+
+	EBT := revenue - expenses
+    profit := EBT * (1-taxRate / 100 )
+	ratio := EBT / profit
+
+	fmt.Println(EBT)
+	fmt.Println(profit)
+	fmt.Println(ratio)
+
+	// fmt.Print("years: ")
+	// fmt.Scan(&years)
+
+	// fmt.Print("Expected Return: ")
+	// fmt.Scan(&expectedReturnRate)
 
 
-    futureValue := investmentAmount * math.Pow(1 + expectedReturnRate /100,years) 
-	futureRealValue :=futureValue / math.Pow(1 + inflationRate /100,years)
+    // futureValue := investmentAmount * math.Pow(1 + expectedReturnRate /100,years) 
+	// futureRealValue :=futureValue / math.Pow(1 + inflationRate /100,years)
 
-    fmt.Println(futureValue)
-    fmt.Println(futureRealValue)
+    // fmt.Println(futureValue)
+    // fmt.Println(futureRealValue)
  }
