04a877a6813ae0312c50a9f1bec00c1580fe57f1
Businge931 <jimmybusinge9@gmail.com>
2024-05-28 15:28:25 +0300

    18. Exercise Building a Profit Calculator

--- investment_calulator.go
+++ investment_calulator.go
@@ -5,11 +5,6 @@ import (
 )
 
 func main(){
-	// const inflationRate =2.5
-	// var investmentAmount float64 
-	// var years float64
-	// var expectedReturnRate float64
-
 	var taxRate float64
 	var revenue float64
 	var expenses float64
@@ -31,16 +26,5 @@ func main(){
 	fmt.Println(profit)
 	fmt.Println(ratio)
 
-	// fmt.Print("years: ")
-	// fmt.Scan(&years)
 
-	// fmt.Print("Expected Return: ")
-	// fmt.Scan(&expectedReturnRate)
-
-
-    // futureValue := investmentAmount * math.Pow(1 + expectedReturnRate /100,years) 
-	// futureRealValue :=futureValue / math.Pow(1 + inflationRate /100,years)
-
-    // fmt.Println(futureValue)
-    // fmt.Println(futureRealValue)
  }
