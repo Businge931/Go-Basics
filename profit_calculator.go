291d6ed61a3fad6f8a33d418bb1057ec5a0555c4
Businge931 <jimmybusinge9@gmail.com>
2024-05-29 15:29:23 +0300

    fix: add return to default of the loop

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,59 +1,59 @@
 package main
 
-import (
-	"fmt"
-	"errors"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"errors"
+// 	"os"
+// )
 
-func main(){
-	revenue, err1 := getUserInput("Revenue: ")
+// func main(){
+// 	revenue, err1 := getUserInput("Revenue: ")
 
-	// if err !=nil{
-	// 	fmt.Println(err)
-	// 	return
-	// }
+// 	// if err !=nil{
+// 	// 	fmt.Println(err)
+// 	// 	return
+// 	// }
 
-	expenses, err2 := getUserInput("Expenses: ")
+// 	expenses, err2 := getUserInput("Expenses: ")
 
-	// if err !=nil{
-	// 	fmt.Println(err)
-	// 	return
-	// }
+// 	// if err !=nil{
+// 	// 	fmt.Println(err)
+// 	// 	return
+// 	// }
 
-	taxRate, err3 := getUserInput("taxRate: ")
+// 	taxRate, err3 := getUserInput("taxRate: ")
 
-	if err1 !=nil || err2 !=nil || err3 !=nil{
-		fmt.Println(err1)
-		return
-	}
+// 	if err1 !=nil || err2 !=nil || err3 !=nil{
+// 		fmt.Println(err1)
+// 		return
+// 	}
 	
-	ebt, profit, ratio := calculateFinancials(revenue,expenses,taxRate)
-	fmt.Printf("%.1f\n",ebt)
-	fmt.Printf("%.1f\n",profit)
-	fmt.Printf("%.3f\n",ratio)
-	storeResults(ebt, profit, ratio)
-}
+// 	ebt, profit, ratio := calculateFinancials(revenue,expenses,taxRate)
+// 	fmt.Printf("%.1f\n",ebt)
+// 	fmt.Printf("%.1f\n",profit)
+// 	fmt.Printf("%.3f\n",ratio)
+// 	storeResults(ebt, profit, ratio)
+// }
 
-func storeResults(ebt,profit,ratio float64){
-	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n",ebt,profit,ratio)
-	os.WriteFile("results.tsx",[]byte(results),0644)
-}
+// func storeResults(ebt,profit,ratio float64){
+// 	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n",ebt,profit,ratio)
+// 	os.WriteFile("results.tsx",[]byte(results),0644)
+// }
 
-func calculateFinancials(revenue,expenses,taxRate float64)(ebt float64,profit float64,ratio float64){
-	ebt =revenue - expenses
-	profit =ebt * (1-taxRate/100)
-	ratio = ebt / profit
-	return ebt, profit, ratio
-}
+// func calculateFinancials(revenue,expenses,taxRate float64)(ebt float64,profit float64,ratio float64){
+// 	ebt =revenue - expenses
+// 	profit =ebt * (1-taxRate/100)
+// 	ratio = ebt / profit
+// 	return ebt, profit, ratio
+// }
 
-func getUserInput(infoText string) (float64, error){
-	var userInput float64
-	fmt.Print(infoText)
-	fmt.Scan(&userInput)
+// func getUserInput(infoText string) (float64, error){
+// 	var userInput float64
+// 	fmt.Print(infoText)
+// 	fmt.Scan(&userInput)
 
-	if userInput <=0{
-		return 0, errors.New("value must be a positive number")
-	}
-    return userInput, nil
-}
+// 	if userInput <=0{
+// 		return 0, errors.New("value must be a positive number")
+// 	}
+//     return userInput, nil
+// }
