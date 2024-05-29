753f3b07248ecf819f732bd5fa8c2719ffbf1aed
Businge931 <jimmybusinge9@gmail.com>
2024-05-29 14:43:02 +0300

    37. Making Sense of switch Statements

--- bank.go
+++ bank.go
@@ -19,6 +19,49 @@ for {
 
 	// wantsCheckBalance := choice == 1
 
+	switch choice {
+	case 1:
+     fmt.Println("Your balance is:",accountBalance)
+	case 2:
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+	    fmt.Scan(&depositAmount)
+
+        if depositAmount <= 0{
+			fmt.Println("Invalid amount. Must be greater that 0")
+			// return
+			continue
+		}
+
+		accountBalance += depositAmount  //accountBalnce = accountBalance + depositAmount
+		fmt.Println("Balance updated! New amount:", accountBalance)
+
+	case 3:	
+	   fmt.Print("Your withdraw amount: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+
+        if withdrawAmount <= 0{
+			fmt.Println("Invalid amount. Must be greater that 0")
+			// return
+			continue
+		}
+
+		if withdrawAmount>accountBalance{
+			fmt.Println("Invalid amount. You can't withdraw more than you have.")
+			// return
+			continue
+		}
+
+		accountBalance -=withdrawAmount
+		fmt.Println("Balance updated! New amount:", accountBalance)
+
+	}
+
+
+
+
+
 	if choice == 1 {
      fmt.Println("Your balance is:",accountBalance)
 	} else if choice ==2 {
@@ -47,7 +90,8 @@ for {
 
 		if withdrawAmount>accountBalance{
 			fmt.Println("Invalid amount. You can't withdraw more than you have.")
-			return
+			// return
+			continue
 		}
 
 		accountBalance -=withdrawAmount
