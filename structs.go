d625cf973c28c3839b13ad67f5a62cede6612870
Businge931 <jimmybusinge9@gmail.com>
2024-05-30 10:07:26 +0300

    5. Instantiating Structs & Struct Literal Notation

--- structs.go
+++ structs.go
@@ -1,13 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName string
+	birthdate string
+	createdAt time.Time
+} 
 
 func main (){
-	firstName :=getUserData("Please enter your first name: ")
-	lastName :=getUserData("Please enter your last name: ")
-	birthdate :=getUserData("Please enter your bithdate (MM/DD/YYYY): ")
+	userfirstName :=getUserData("Please enter your first name: ")
+	userlastName :=getUserData("Please enter your last name: ")
+	userbirthdate :=getUserData("Please enter your bithdate (MM/DD/YYYY): ")
 
-	fmt.Println(firstName,lastName,birthdate)
+
+var appUser user
+
+appUser = user{
+	firstName: userfirstName, 
+	lastName: userlastName,
+	birthdate: userbirthdate, 
+	createdAt: time.Now(),
+}
+
+	fmt.Println(userlastName,userfirstName,userbirthdate)
 }
 
 func getUserData(promptText string) string{
