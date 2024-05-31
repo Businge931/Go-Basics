17a1a7a9d6bf875a589b2d0a3c1085c40ebbef46
Businge931 <jimmybusinge9@gmail.com>
2024-05-31 09:12:31 +0300

    15. Exposing Methods & A Different Constructor Function Name

--- structs.go
+++ structs.go
@@ -1,39 +1,8 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"time"
-)
+import "fmt"
 
-type user struct {
-	firstName string
-	lastName string
-	birthdate string
-	createdAt time.Time
-} 
-
-func (u * user) outputUserDetails (){
-fmt.Println(u.firstName,u.lastName,u.birthdate)
-}
-
-func (u *user) clearUserName(){
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName,birthdate string) (*user,error){
-    if firstName =="" || lastName == "" || birthdate =="" {
-		 return nil, errors.New("names and birthdate required!")
-	}
-
-	return &user{
-	firstName: firstName, 
-	lastName: lastName,
-	birthdate: birthdate, 
-	createdAt: time.Now(),
-  }, nil
-}
+import "example.com/structs/user"
 
 func main (){
 	userfirstName :=getUserData("Please enter your first name: ")
@@ -41,17 +10,18 @@ func main (){
 	userbirthdate :=getUserData("Please enter your bithdate (MM/DD/YYYY): ")
 
 
-  var appUser *user
+  var appUser *user.User
 
-  appUser,err := newUser(userlastName,userfirstName,userbirthdate)
+  appUser,err := user.NewUser(userlastName,userfirstName,userbirthdate)
 
  if err != nil {
 	fmt.Println(err)
 	return
  }
 
- appUser.outputUserDetails()
- appUser.clearUserName()
+ appUser.OutputUserDetails()
+ appUser.ClearUserName()
+ appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string{
