8804b87bf8ece0e4d670cbc3736d0e5843a048fd
Businge931 <jimmybusinge9@gmail.com>
2024-06-01 15:43:48 +0300

    formartting

--- user/user.go
+++ user/user.go
@@ -8,48 +8,48 @@ import (
 
 type User struct {
 	firstName string
-	lastName string
+	lastName  string
 	birthdate string
 	createdAt time.Time
-} 
-
-func (u * User) OutputUserDetails (){
-fmt.Println(u.firstName,u.lastName,u.birthdate)
 }
 
-func (u *User) ClearUserName(){
+func (u *User) OutputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func (u *User) ClearUserName() {
 	u.firstName = ""
 	u.lastName = ""
 }
 
-type Admin struct{
-	email string
+type Admin struct {
+	email    string
 	password string
 	User
 }
 
-func NewAdmin(email, password string) Admin{
+func NewAdmin(email, password string) Admin {
 	return Admin{
-		email:email,
+		email:    email,
 		password: password,
 		User: User{
 			firstName: "ADMIN",
-			lastName: "ADMIN",
-			birthdate: "---", 
+			lastName:  "ADMIN",
+			birthdate: "---",
 			createdAt: time.Now(),
 		},
 	}
 }
 
-func NewUser(firstName, lastName,birthdate string) (*User,error){
-    if firstName =="" || lastName == "" || birthdate =="" {
-		 return nil, errors.New("names and birthdate required")
+func NewUser(firstName, lastName, birthdate string) (*User, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("names and birthdate required")
 	}
 
 	return &User{
-	firstName: firstName, 
-	lastName: lastName,
-	birthdate: birthdate, 
-	createdAt: time.Now(),
-  }, nil
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}, nil
 }
