21639754fc2c9e49b7a65c65f53133922b8bd717
Businge931 <jimmybusinge9@gmail.com>
2024-06-04 12:05:56 +0300

    24. Practice Project Encoding JSON Content

--- main.go
+++ main.go
@@ -21,6 +21,13 @@ func main() {
 		return
 	}
 	userNote.Display()
+	err = userNote.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed")
+	}
+
+	fmt.Println("Saving the note succeded")
 
 }
 
@@ -34,14 +41,14 @@ func getNoteData() (string, string) {
 func getUserInput(prompt string) string {
 	fmt.Print(prompt)
 	reader := bufio.NewReader(os.Stdin)
-	text,err := reader.ReadString('\n')
+	text, err := reader.ReadString('\n')
 
-	if err !=nil {
+	if err != nil {
 		return ""
 	}
 
-	text = strings.TrimSuffix(text,"\n")
-	text = strings.TrimSuffix(text,"\r")
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
 
 	return text
 }
