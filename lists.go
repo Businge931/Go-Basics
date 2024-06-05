aaeb9a1a275cb10133344d74bb81f17e51991c78
Businge931 <jimmybusinge9@gmail.com>
2024-06-05 10:28:35 +0300

    4. Selecting Parts of Arrays With Slices

--- lists.go
+++ lists.go
@@ -5,6 +5,16 @@ import "fmt"
 type Product struct{}
 
 func main() {
+	var productNames [4]string = [4]string{"A book"}
 	prices := [4]float64{10.99, 9.99, 20.00, 45.80}
 	fmt.Println(prices)
+
+	productNames[2] = "A carpet"
+	// fmt.Println(productNames)
+
+	fmt.Println(prices[2])
+	featuredPrices := prices[1:3]
+	fmt.Println(featuredPrices)
+
+
 }
