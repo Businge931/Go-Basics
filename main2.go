d297cb1c258fe2a336c7b3448b2c44b86d129c7b
Businge931 <jimmybusinge9@gmail.com>
2024-06-05 18:07:20 +0300

    2. Functions as Values & Function Types

--- main2.go
+++ main2.go
@@ -1,40 +1,40 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-type floatMap map[string]float64
+// type floatMap map[string]float64
 
-func (m floatMap) output() {
-	fmt.Println(m)
-}
+// func (m floatMap) output() {
+// 	fmt.Println(m)
+// }
 
-func main() {
-	userNames := make([]string, 2, 5)
-	userNames = append(userNames, "Jim")
+// func main() {
+// 	userNames := make([]string, 2, 5)
+// 	userNames = append(userNames, "Jim")
 
-	userNames[0] = "Cathy" //But without the make function, this would fail
+// 	userNames[0] = "Cathy" //But without the make function, this would fail
 
-	fmt.Println(userNames)
+// 	fmt.Println(userNames)
 
-	courseRatings := make(floatMap, 3)
+// 	courseRatings := make(floatMap, 3)
 
-	courseRatings["go"] = 4.7
-	courseRatings["react"] = 4.8
-	courseRatings["node"] = 4.8
+// 	courseRatings["go"] = 4.7
+// 	courseRatings["react"] = 4.8
+// 	courseRatings["node"] = 4.8
 
-	// fmt.Println(courseRatings)
-	courseRatings.output()
+// 	// fmt.Println(courseRatings)
+// 	courseRatings.output()
 
-	for index, value := range userNames {
-		// ...
-		fmt.Println("Index:", index)
-		fmt.Println("Value:",value)
-	}
+// 	for index, value := range userNames {
+// 		// ...
+// 		fmt.Println("Index:", index)
+// 		fmt.Println("Value:",value)
+// 	}
 
-	for key, value := range courseRatings{
-		// ...
-		fmt.Println("Key:", key)
-		fmt.Println("Value:",value)
-	}
+// 	for key, value := range courseRatings{
+// 		// ...
+// 		fmt.Println("Key:", key)
+// 		fmt.Println("Value:",value)
+// 	}
 
-}
+// }
