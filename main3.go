1cdcdd3e5d53b736256518924c344a16243fcacd
Businge931 <jimmybusinge9@gmail.com>
2024-06-05 18:59:28 +0300

    Returning functions as values

--- main3.go
+++ main3.go
@@ -6,10 +6,21 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers :=[]int{5,1,2,6}
 	doubled := transformNumbers(&numbers, double)
 	trippled := transformNumbers(&numbers, tripple)
+
 	fmt.Println(doubled)
 	fmt.Println(trippled)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers :=transformNumbers(&numbers,transformerFn1)
+	moreTransformedNumbers :=transformNumbers(&numbers,transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -17,8 +28,15 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 	for _, val := range *numbers {
 		dNumbers = append(dNumbers, transform(val))
 	}
-
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0]==1{
+		return double
+	}else {
+		return tripple
+	}
 }
 
 func double(number int) int {
