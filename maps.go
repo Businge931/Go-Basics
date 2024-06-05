25f36f61799c8a4e9b324c65ae43b288a2c7bf7a
Businge931 <jimmybusinge9@gmail.com>
2024-06-05 12:12:21 +0300

    11. Introducing Maps

---
+++ maps.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google":              "https://google.com",
+		"Amazon web services": "https://aws.com",
+	}
+
+	fmt.Println(websites)
+}
