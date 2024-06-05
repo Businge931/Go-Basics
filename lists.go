dfaa4fc96c81781373984f12024cc6b786de5d4b
Businge931 <jimmybusinge9@gmail.com>
2024-06-05 11:35:58 +0300

    7. Exercise solution

--- lists.go
+++ lists.go
@@ -2,20 +2,32 @@ package main
 
 import "fmt"
 
-type Product struct{}
-
 func main() {
-	var productNames [4]string = [4]string{"A book"}
-	prices := [4]float64{10.99, 9.99, 20.00, 45.80}
-	fmt.Println(prices)
+	// prices := []float64{10.99, 8.99}
+	// fmt.Println(prices)
 
-	productNames[2] = "A carpet"
-	// fmt.Println(productNames)
+	// updatedPRICES:= append(prices,5.6)
+	// fmt.Println(updatedPRICES)
 
-	fmt.Println(prices[2])
-	featuredPrices := prices[:3]
-	// featuredPrices = prices[1:]
-	fmt.Println(featuredPrices)
+	hobbies :=[3]string{"Chilling","Watching series","Good company"}
+	fmt.Println(hobbies)
 
+	fmt.Println(hobbies[0])
+	fmt.Println(hobbies[1:])
 
 }
+
+// func main() {
+// 	var productNames [4]string = [4]string{"A book"}
+// 	prices := [4]float64{10.99, 9.99, 20.00, 45.80}
+// 	fmt.Println(prices)
+
+// 	productNames[2] = "A carpet"
+// 	// fmt.Println(productNames)
+
+// 	fmt.Println(prices[2])
+// 	featuredPrices := prices[:3]
+// 	// featuredPrices = prices[1:]
+// 	fmt.Println(featuredPrices)
+
+// }
