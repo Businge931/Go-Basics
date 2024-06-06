b416933549ea7cced47c025f2e83d990c244915b
Businge931 <jimmybusinge9@gmail.com>
2024-06-06 10:44:56 +0300

    4. Introducing Anonymous Functions

--- main3.go
+++ main3.go
@@ -1,47 +1,47 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-type transformFn func(int) int
+// type transformFn func(int) int
 
-func main() {
-	numbers := []int{1, 2, 3, 4}
-	moreNumbers :=[]int{5,1,2,6}
-	doubled := transformNumbers(&numbers, double)
-	trippled := transformNumbers(&numbers, tripple)
+// func main() {
+// 	numbers := []int{1, 2, 3, 4}
+// 	moreNumbers :=[]int{5,1,2,6}
+// 	doubled := transformNumbers(&numbers, double)
+// 	trippled := transformNumbers(&numbers, tripple)
 
-	fmt.Println(doubled)
-	fmt.Println(trippled)
+// 	fmt.Println(doubled)
+// 	fmt.Println(trippled)
 
-	transformerFn1 := getTransformerFunction(&numbers)
-	transformerFn2 := getTransformerFunction(&moreNumbers)
+// 	transformerFn1 := getTransformerFunction(&numbers)
+// 	transformerFn2 := getTransformerFunction(&moreNumbers)
 
-	transformedNumbers :=transformNumbers(&numbers,transformerFn1)
-	moreTransformedNumbers :=transformNumbers(&numbers,transformerFn2)
+// 	transformedNumbers :=transformNumbers(&numbers,transformerFn1)
+// 	moreTransformedNumbers :=transformNumbers(&numbers,transformerFn2)
 
-	fmt.Println(transformedNumbers)
-	fmt.Println(moreTransformedNumbers)
-}
+// 	fmt.Println(transformedNumbers)
+// 	fmt.Println(moreTransformedNumbers)
+// }
 
-func transformNumbers(numbers *[]int, transform transformFn) []int {
-	dNumbers := []int{}
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transform(val))
-	}
-	return dNumbers
-}
+// func transformNumbers(numbers *[]int, transform transformFn) []int {
+// 	dNumbers := []int{}
+// 	for _, val := range *numbers {
+// 		dNumbers = append(dNumbers, transform(val))
+// 	}
+// 	return dNumbers
+// }
 
-func getTransformerFunction(numbers *[]int) transformFn {
-	if (*numbers)[0]==1{
-		return double
-	}else {
-		return tripple
-	}
-}
+// func getTransformerFunction(numbers *[]int) transformFn {
+// 	if (*numbers)[0]==1{
+// 		return double
+// 	}else {
+// 		return tripple
+// 	}
+// }
 
-func double(number int) int {
-	return number * 2
-}
-func tripple(number int) int {
-	return number * 3
-}
+// func double(number int) int {
+// 	return number * 2
+// }
+// func tripple(number int) int {
+// 	return number * 3
+// }
