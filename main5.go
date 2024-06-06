8c863c3a8a2da3177c9d1f4a583bf00c0ae17b98
Businge931 <jimmybusinge9@gmail.com>
2024-06-06 11:20:07 +0300

    7. Using Variadic Functions

--- main5.go
+++ main5.go
@@ -1,15 +1,15 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	fact :=factorial(5)
-	fmt.Println(fact)
-}
+// func main() {
+// 	fact :=factorial(5)
+// 	fmt.Println(fact)
+// }
 
-func factorial(number int) int {
-	if number == 0 {
-		return 1
-	}
-	return number * factorial(number-1)
-}
+// func factorial(number int) int {
+// 	if number == 0 {
+// 		return 1
+// 	}
+// 	return number * factorial(number-1)
+// }
---
+++ main6.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// numbers := []int{3, 6, 7, 9, 5, 2}
+	total := sumUp(3, 6, 7, 9, 5, 2)
+
+	fmt.Println(total)
+}
+
+func sumUp(numbers ...int) int {
+	sum := 0
+
+	for _, val := range numbers {
+		sum += val
+	}
+	return sum
+}
